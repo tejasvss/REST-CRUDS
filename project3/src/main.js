16dbea4a9ca0ff70861b51da6a79f8d3291162e8
seguteja <sivateja@skystop.tech>
2021-07-08 12:44:26 +0530

    Second Commit

--- project3/src/main.js
+++ project3/src/main.js
@@ -21,17 +21,7 @@ app.listen(port,(()=>{
 
 
 
-//USer URL
 
-// app.post('/user',(req,res)=>{
-//     const user=new User(req.body);
-
-//     user.save().then(()=>{
-//         res.send(user);
-//     }).catch((e)=>{
-//         res.status(400).send(e)
-//     })
-// });
 
 
 
